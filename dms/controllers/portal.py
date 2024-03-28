OCA/dms

[IMP] dms: Inherit permissions from related records

dms/controllers/portal.py
@@ -16,24 +16,22 @@ _logger = logging.getLogger(__name__)
 class CustomerPortal(CustomerPortal):
     def _dms_check_access(self, model, res_id, access_token=None):
         try:
-            item_sudo = request.env[model].sudo().browse([res_id])
+            item = request.env[model].browse(res_id)
         except (AccessError, MissingError):
             return False
 
         if access_token:
-            if not item_sudo.check_access_token(access_token):
+            if not item.check_access_token(access_token):
                 return False
         else:
-            if not item_sudo.sudo(request.env.user.id).check_access("read", False):
+            if not item.sudo(request.env.user.id).check_access("read", False):
                 return False
 
-        return item_sudo
+        return item
 
     def _prepare_portal_layout_values(self):
         values = super()._prepare_portal_layout_values()
-        ids = request.env["dms.directory"]._get_own_root_directories(
-            request.env.user.id
-        )
+        ids = request.env["dms.directory"]._get_own_root_directories()
         values.update({"dms_directory_count": len(ids)})
         return values
 
@@ -57,13 +55,7 @@ class CustomerPortal(CustomerPortal):
             filterby = "name"
         # domain
         domain = [
-            (
-                "id",
-                "in",
-                request.env["dms.directory"]._get_own_root_directories(
-                    request.env.user.id
-                ),
-            )
+            ("id", "in", request.env["dms.directory"]._get_own_root_directories(),)
         ]
         # search
         if search and search_in:
@@ -81,7 +73,7 @@ class CustomerPortal(CustomerPortal):
         # values
         values.update(
             {
-                "dms_directories": items.sudo(),
+                "dms_directories": items,
                 "page_name": "dms_directory",
                 "default_url": "/my/dms",
                 "searchbar_sortings": searchbar_sortings,
@@ -169,7 +161,7 @@ class CustomerPortal(CustomerPortal):
         )._get_parent_categories(access_token)
         # values
         values = {
-            "dms_directories": dms_directory_items.sudo(),
+            "dms_directories": dms_directory_items,
             "page_name": "dms_directory",
             "default_url": "/my/dms/directories",
             "searchbar_sortings": searchbar_sortings,
@@ -179,7 +171,7 @@ class CustomerPortal(CustomerPortal):
             "filterby": filterby,
             "access_token": access_token,
             "dms_directory": dms_directory_sudo,
-            "dms_files": dms_file_items.sudo(),
+            "dms_files": dms_file_items,
             "dms_parent_categories": dms_parent_categories,
         }
         return request.render("dms.portal_my_dms", values)
@@ -193,12 +185,6 @@ class CustomerPortal(CustomerPortal):
     def portal_my_dms_file_download(self, dms_file_id, access_token=None, **kw):
         """Process user's consent acceptance or rejection."""
         ensure_db()
-        try:
-            # If there's a website, we need a user to render the template
-            request.uid = request.website.user_id.id
-        except AttributeError:
-            # If there's no website, the default is OK
-            pass
         # operations
         res = self._dms_check_access("dms.file", dms_file_id, access_token)
         if not res:

dms/tests/test_storage_attachment.py
@@ -1,0 +1,62 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from .common import DocumentsBaseCase
+
+
+class StorageAttachmentTestCase(DocumentsBaseCase):
+    def setUp(self):
+        super().setUp()
+        self.storage = self.browse_ref("dms.storage_attachment_demo")
+        self.model_res_partner = self.browse_ref("base.model_res_partner")
+        self.partner = self.browse_ref("base.res_partner_12")
+
+    def _create_attachment(self, name, uid):
+        self.create_attachment(
+            name=name,
+            res_model=self.model_res_partner.model,
+            res_id=self.partner.id,
+            sudo=False,
+        ).sudo(uid)
+
+    def test_storage_attachment(self):
+        self._create_attachment("demo.txt", self.admin_uid)
+        self.assertEqual(self.storage.count_storage_files, 1)
+        directory_id = self.directory.sudo(self.admin_uid).search(
+            [
+                ("storage_id", "=", self.storage.id),
+                ("res_model", "=", self.model_res_partner.model),
+                ("res_id", "=", self.partner.id),
+            ]
+        )
+        self.assertEqual(directory_id.count_files, 1)
+        self.assertEqual(directory_id.file_ids[0].name, "demo.txt")
+        file_01 = self.create_file_with_context(
+            context={"default_res_model": self.model_res_partner.model},
+            directory=directory_id,
+            storage=directory_id.storage_id,
+        ).sudo(self.admin_uid)
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "attachment")
+        self.assertEqual(file_01.save_type, "database")
+        self.assertEqual(self.storage.count_storage_files, 2)
+
+    def test_storage_attachment_directory_record_ref_access(self):
+        self._create_attachment("demo.txt", self.admin_uid)
+        directory_id = self.directory.sudo(self.admin_uid).search(
+            [
+                ("storage_id", "=", self.storage.id),
+                ("res_model", "=", self.model_res_partner.model),
+                ("res_id", "=", self.partner.id),
+            ]
+        )
+        self.assertTrue(directory_id.sudo(self.admin_uid).check_access("read"))
+        # demo can access res_partner_12
+        self.browse_ref("base.user_demo").write(
+            {"groups_id": [(2, self.browse_ref("dms.group_dms_user").id, False)]}
+        )
+        self.assertEqual(self.partner.type, "contact")
+        self.assertTrue(directory_id.sudo(self.demo_uid).check_access("read"))
+        self.partner.sudo().write({"type": "private"})
+        self.assertEqual(self.partner.type, "private")
+        self.assertFalse(directory_id.sudo(self.demo_uid).check_access("read"))

tva-subteno-it/dms_test

dms/controllers/portal.py
@@ -16,24 +16,22 @@ _logger = logging.getLogger(__name__)
 class CustomerPortal(CustomerPortal):
     def _dms_check_access(self, model, res_id, access_token=None):
         try:
-            item_sudo = request.env[model].sudo().browse([res_id])
+            item = request.env[model].browse(res_id)
         except (AccessError, MissingError):
             return False
 
         if access_token:
-            if not item_sudo.check_access_token(access_token):
+            if not item.check_access_token(access_token):
                 return False
         else:
-            if not item_sudo.with_user(request.env.user.id).check_access("read", False):
+            if not item.with_user(request.env.user.id).check_access("read", False):
                 return False
 
-        return item_sudo
+        return item
 
     def _prepare_portal_layout_values(self):
         values = super()._prepare_portal_layout_values()
-        ids = request.env["dms.directory"]._get_own_root_directories(
-            request.env.user.id
-        )
+        ids = request.env["dms.directory"]._get_own_root_directories()
         values.update({"dms_directory_count": len(ids)})
         return values
 
@@ -57,13 +55,7 @@ class CustomerPortal(CustomerPortal):
             filterby = "name"
         # domain
         domain = [
-            (
-                "id",
-                "in",
-                request.env["dms.directory"]._get_own_root_directories(
-                    request.env.user.id
-                ),
-            )
+            ("id", "in", request.env["dms.directory"]._get_own_root_directories(),)
         ]
         # search
         if search and search_in:
@@ -81,7 +73,7 @@ class CustomerPortal(CustomerPortal):
         # values
         values.update(
             {
-                "dms_directories": items.sudo(),
+                "dms_directories": items,
                 "page_name": "dms_directory",
                 "default_url": "/my/dms",
                 "searchbar_sortings": searchbar_sortings,
@@ -169,7 +161,7 @@ class CustomerPortal(CustomerPortal):
         )._get_parent_categories(access_token)
         # values
         values = {
-            "dms_directories": dms_directory_items.sudo(),
+            "dms_directories": dms_directory_items,
             "page_name": "dms_directory",
             "default_url": "/my/dms",
             "searchbar_sortings": searchbar_sortings,
@@ -179,7 +171,7 @@ class CustomerPortal(CustomerPortal):
             "filterby": filterby,
             "access_token": access_token,
             "dms_directory": dms_directory_sudo,
-            "dms_files": dms_file_items.sudo(),
+            "dms_files": dms_file_items,
             "dms_parent_categories": dms_parent_categories,
         }
         return request.render("dms.portal_my_dms", values)
@@ -193,12 +185,6 @@ class CustomerPortal(CustomerPortal):
     def portal_my_dms_file_download(self, dms_file_id, access_token=None, **kw):
         """Process user's consent acceptance or rejection."""
         ensure_db()
-        try:
-            # If there's a website, we need a user to render the template
-            request.uid = request.website.user_id.id
-        except AttributeError:
-            # If there's no website, the default is OK
-            pass
         # operations
         res = self._dms_check_access("dms.file", dms_file_id, access_token)
         if not res:

dms/tests/test_storage_attachment.py
@@ -1,0 +1,60 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from .common import DocumentsBaseCase
+
+
+class StorageAttachmentTestCase(DocumentsBaseCase):
+    def setUp(self):
+        super().setUp()
+        self.storage = self.browse_ref("dms.storage_attachment_demo")
+        self.model_res_partner = self.browse_ref("base.model_res_partner")
+        self.partner = self.browse_ref("base.res_partner_12")
+
+    def _create_attachment(self, name, uid):
+        self.create_attachment(
+            name=name,
+            res_model=self.model_res_partner.model,
+            res_id=self.partner.id,
+            sudo=False,
+        ).with_user(uid)
+
+    def test_storage_attachment(self):
+        self._create_attachment("demo.txt", self.admin_uid)
+        self.assertEqual(self.storage.count_storage_files, 1)
+        directory_id = self.directory.with_user(self.admin_uid).search(
+            [
+                ("storage_id", "=", self.storage.id),
+                ("res_model", "=", self.model_res_partner.model),
+                ("res_id", "=", self.partner.id),
+            ]
+        )
+        self.assertEqual(directory_id.count_files, 1)
+        self.assertEqual(directory_id.file_ids[0].name, "demo.txt")
+        file_01 = self.create_file(
+            directory=directory_id, storage=directory_id.storage_id,
+        ).with_user(self.admin_uid)
+        self.assertEqual(file_01.storage_id, self.storage)
+        self.assertEqual(file_01.storage_id.save_type, "attachment")
+        self.assertEqual(file_01.save_type, "database")
+        self.assertEqual(self.storage.count_storage_files, 2)
+
+    def test_storage_attachment_directory_record_ref_access(self):
+        self._create_attachment("demo.txt", self.admin_uid)
+        directory_id = self.directory.with_user(self.admin_uid).search(
+            [
+                ("storage_id", "=", self.storage.id),
+                ("res_model", "=", self.model_res_partner.model),
+                ("res_id", "=", self.partner.id),
+            ]
+        )
+        self.assertTrue(directory_id.with_user(self.admin_uid).check_access("read"))
+        # demo can access res_partner_12
+        self.browse_ref("base.user_demo").write(
+            {"groups_id": [(2, self.browse_ref("dms.group_dms_user").id, False)]}
+        )
+        self.assertEqual(self.partner.type, "contact")
+        self.assertTrue(directory_id.with_user(self.demo_uid).check_access("read"))
+        self.partner.sudo().write({"type": "private"})
+        self.assertEqual(self.partner.type, "private")
+        self.assertFalse(directory_id.with_user(self.demo_uid).check_access("read"))