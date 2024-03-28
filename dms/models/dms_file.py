OCA/dms

[FIX] dms: Define the file extension correctly (even if there is no extension in the file name).

dms/models/dms_file.py
@@ -373,10 +373,12 @@ class File(models.Model):
                 }
             )
 
-    @api.depends("name")
+    @api.depends("name", "res_mimetype", "content")
     def _compute_extension(self):
         for record in self:
-            record.extension = file.guess_extension(record.name)
+            record.extension = file.guess_extension(
+                record.name, record.res_mimetype, record.content
+            )
 
     @api.depends("content")
     def _compute_mimetype(self):

tva-subteno-it/dms_test

dms/models/dms_file.py
@@ -376,10 +376,12 @@ class File(models.Model):
                 }
             )
 
-    @api.depends("name")
+    @api.depends("name", "mimetype", "content")
     def _compute_extension(self):
         for record in self:
-            record.extension = file.guess_extension(record.name)
+            record.extension = file.guess_extension(
+                record.name, record.mimetype, record.content
+            )
 
     @api.depends("content")
     def _compute_mimetype(self):