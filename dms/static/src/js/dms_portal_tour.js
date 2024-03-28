OCA/dms

[MIG] dms: Migration to 14.0

dms/static/src/js/dms_portal_tour.js
@@ -15,10 +15,10 @@ odoo.define("dms.tour", function (require) {
                 trigger: 'a[href*="/my/dms"]',
             },
             {
-                content: "Go to Photos directory",
-                trigger: ".tr_dms_directory_link:eq(1)",
+                content: "Go to Documents directory",
+                extra_trigger: "li.breadcrumb-item:contains('DMS')",
+                trigger: ".tr_dms_directory_link:eq(0)",
             },
         ]
     );
-    return {};
 });

tva-subteno-it/dms_test

dms/static/src/js/dms_portal_tour.js
@@ -15,10 +15,10 @@ odoo.define("dms.tour", function (require) {
                 trigger: 'a[href*="/my/dms"]',
             },
             {
-                content: "Go to Photos directory",
-                trigger: ".tr_dms_directory_link:eq(1)",
+                content: "Go to Documents directory",
+                extra_trigger: "li.breadcrumb-item:contains('Documents')",
+                trigger: ".tr_dms_directory_link:eq(0)",
             },
         ]
     );
-    return {};
 });